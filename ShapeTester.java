4c51312d610262b36a26d245b82282b9a3155fbb
Laura-Neff <ljneff@gmail.com>
2019-09-21 20:34:11 -0400

    Initialized files

---
+++ ShapeTester.java
@@ -0,0 +1,37 @@
+package ShapeRepresentations;
+
+public class ShapeTester {
+
+  // TODO: [2 points] Write the method isLarger which takes as input two shapes
+  // (a ShapeRepresentations.Circle first, then a ShapeRepresentations.Rectangle) and returns true if the area of
+  // the circle is bigger than (or equal to) the area of the rectangle,
+  // false otherwise.
+  // ... Add your code here ... //
+
+  // TODO: [2 points] Write the method longerPerim which takes as input
+  // a ShapeRepresentations.Circle object followed by a ShapeRepresentations.Rectangle object and returns
+  // the length of the perimeter of the longer of the two objects.
+  // ... Add your code here ... //
+
+  // TODO: [2 points] Write another version of the method longerPerim which has the
+  // same name and functionality but it takes as input a ShapeRepresentations.Rectangle object
+  // followed by a ShapeRepresentations.Circle object. The method also returns the length of
+  // the perimeter of the longer of the two objects.
+  // ... Add your code here ... //
+
+  // TODO: [2 points] Write the method largerArea which takes as input
+  // a ShapeRepresentations.Circle object followed by a ShapeRepresentations.Rectangle object and returns
+  // the area of the larger of the two objects.
+  // ... Add your code here ... //
+
+  // TODO: [2 points] Writer another version of the method largerArea which has the
+  // same name and functionality but it takes as input a ShapeRepresentations.Rectangle object
+  // followed by a ShapeRepresentations.Circle object.
+  // ... Add your code here ... //
+
+  // TODO: [10 points] Write the method containsCenter which takes as input two circles,
+  // and returns true if the first circle contains the center of the second circle,
+  // false otherwise.
+  // ... Add your code here ... //
+
+}
